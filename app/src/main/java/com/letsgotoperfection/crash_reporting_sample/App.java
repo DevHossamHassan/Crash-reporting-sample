60f287e69ef0ad7145af71ba01858284eda17c7f
DevHossamHassan <hossamhassancs15@gmail.com>
2017-07-25 16:41:15 +0200

    Initial commit

---
+++ app/src/main/java/com/letsgotoperfection/crash_reporting_sample/App.java
@@ -0,0 +1,14 @@
+package com.letsgotoperfection.crash_reporting_sample;
+
+import android.app.Application;
+
+/**
+ * @author hossam.
+ */
+
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
